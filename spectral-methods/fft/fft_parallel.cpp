d3f4ad626edc554f05b161e6c32e270ad3b72324
demingri21 <demingri21@vt.edu>
2025-11-17 14:56:54 -0500

    parallelized fft_parallel

--- spectral-methods/fft/fft_parallel.cpp
+++ spectral-methods/fft/fft_parallel.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
 #include <cstdint>
+#include <omp.h>
+#include <cstdio>
 
 // Intentionally identical to serial; students will parallelize later.
 void fft_parallel(float* __restrict real_op,
@@ -7,15 +9,21 @@ void fft_parallel(float* __restrict real_op,
                   int N,
                   const float* __restrict real_weight,
                   const float* __restrict imag_weight,
-                  int /*nthreads*/)
+                  int nthreads)
 {
-    int iters = 0; { int t = N; while (t > 1) { t >>= 1; ++iters; } }
+    int iters = 0; { int t = N; while (t > 1) { t >>= 1; ++iters; } } //this computes number of FFT stages
     int n = 1;
     int a = N / 2;
-
-    for (int j = 0; j < iters; ++j) {
-        for (int i = 0; i < N; ++i) {
-            if (!(i & n)) {
+    for (int j = 0; j < iters; ++j)  //can't parallelize here becuase each iteration depends on previous one
+    {
+        #pragma omp parallel for schedule(guided) num_threads(nthreads)
+        for (int i = 0; i < N; ++i)
+        {
+            //n is always a power of 2
+            //if statement checks the log(n)th bit of i
+            if (!(i & n))
+            {
+                //this block actually computes the fourier transform, "returns" output in real_op and imag_op
                 std::size_t op_index = ( (static_cast<std::size_t>(i) * static_cast<std::size_t>(a)) %
                                          (static_cast<std::size_t>(n) * static_cast<std::size_t>(a)) );
                 int res_index = i + n;
